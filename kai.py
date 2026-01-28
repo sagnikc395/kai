ce565c8c5c74e4d4c333d1950f8dd8caee7e9d44
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-27 23:26:09 -0500

    feat: passed prompt as argument

--- kai.py
+++ kai.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 
 def auth():
@@ -12,17 +14,27 @@ def auth():
 
 def main():
     api_key = auth()
+    if api_key is None:
+        print("ERR: Please setup an api key before proceeding")
+        sys.exit(1)
     print(f"API key: ${api_key}")
+
+    if len(sys.argv) < 2:
+        print("ERR: Need a prompt!")
+        sys.exit(1)
+    prompt = sys.argv[1]
     client = genai.Client(api_key=api_key)
 
+    
+
     response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents="i want to organize the worlds's knowledge into a few bytes, what should i convey ?",
+        model="gemini-3-flash-preview", contents=prompt.strip()
     )
 
     print(response.text)
     if response is None or response.usage_metadata is None:
-        return
+        print("ERR: Response is malformed")
+        sys.exit(1)
     print(f"Prompt tokens : {response.usage_metadata.prompt_token_count}")  # type: ignore
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # type: ignore
 
