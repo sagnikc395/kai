2334d944b8d9dd083dbc209dbbe5d6545aea9467
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-28 11:22:23 -0500

    feat: added writing file

---
+++ kai/write_file.py
@@ -0,0 +1,23 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f"Error: '{file_path}' is not in the working directory"
+    if not os.path.isfile(abs_file_path):
+        parent_dir = os.path.dirname(abs_file_path)
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"Could not create parent dirs: {parent_dir} = {e}"
+
+    try:
+        with open(file_path, "w") as f:
+            f.write(content)
+        return (
+            f"Successfully wrote to '{file_path}' ({len(content)} characters written)"
+        )
+    except Exception as e:
+        return f"Failed to write to file : {file_path}, {e}"
