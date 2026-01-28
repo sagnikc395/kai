ba858e01803b9ef2f326842c5bf9413dec313b4d
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-27 20:16:08 -0500

    feat: basics of what is to be a great agent

--- kai.py
+++ kai.py
@@ -12,8 +12,19 @@ def auth():
 
 def main():
     api_key = auth()
+    print(f"API key: ${api_key}")
     client = genai.Client(api_key=api_key)
-    
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="i want to organize the worlds's knowledge into a few bytes, what should i convey ?",
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        return
+    print(f"Prompt tokens : {response.usage_metadata.prompt_token_count}")  # type: ignore
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  # type: ignore
 
 
 if __name__ == "__main__":
