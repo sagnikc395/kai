b52b9f3b73397198765d5a7ddadb4afbbfb17a43
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-02-01 13:36:27 -0500

    cleanup of if able to make parent dir

--- kai/write_file.py
+++ kai/write_file.py
@@ -6,13 +6,15 @@ def write_file(working_directory, file_path, content):
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     if not abs_file_path.startswith(abs_working_dir):
         return f"Error: '{file_path}' is not in the working directory"
-    if not os.path.isfile(abs_file_path):
-        parent_dir = os.path.dirname(abs_file_path)
+    parent_dir = os.path.dirname(abs_file_path)
+
+    if not os.path.isdir(parent_dir):
         try:
             os.makedirs(parent_dir)
         except Exception as e:
             return f"Could not create parent dirs: {parent_dir} = {e}"
-
+    if not os.path.isfile(abs_file_path):
+        pass
     try:
         with open(file_path, "w") as f:
             f.write(content)
